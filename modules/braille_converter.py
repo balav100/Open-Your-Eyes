bb6ffb71babe32b81c6f264cbbc09a2a86f9a151
Balasubramaniam V <balav6566@outlook.com>
2026-08-24 22:57:50 +0530

    Refactor braille conversion logic and mappings

--- modules/braille_converter.py
+++ modules/braille_converter.py
@@ -1,4 +1,4 @@
-BRAILLE_MAP = {
+BRAILLE_LETTERS = {
     'a': '⠁',
     'b': '⠃',
     'c': '⠉',
@@ -24,20 +24,79 @@ BRAILLE_MAP = {
     'w': '⠺',
     'x': '⠭',
     'y': '⠽',
-    'z': '⠵',
-    ' ': ' '
+    'z': '⠵'
 }
+
+
+BRAILLE_NUMBERS = {
+    '1': '⠼⠁',
+    '2': '⠼⠃',
+    '3': '⠼⠉',
+    '4': '⠼⠙',
+    '5': '⠼⠑',
+    '6': '⠼⠋',
+    '7': '⠼⠛',
+    '8': '⠼⠓',
+    '9': '⠼⠊',
+    '0': '⠼⠚'
+}
+
+
+BRAILLE_PUNCTUATION = {
+    ' ': ' ',
+    ',': '⠂',
+    ';': '⠆',
+    ':': '⠒',
+    '.': '⠲',
+    '!': '⠖',
+    '?': '⠦',
+    "'": '⠄',
+    '-': '⠤',
+    '(': '⠷',
+    ')': '⠾',
+    '"': '⠶',
+    '/': '⠌'
+}
+
+
+CAPITAL_SIGN = '⠠'
 
 
 def text_to_braille(text):
 
     braille = ""
 
-    for char in text.lower():
+    number_mode = False
 
-        braille += BRAILLE_MAP.get(
-            char,
-            char
-        )
+    for char in text:
+
+        lower_char = char.lower()
+
+        if char.isupper() and lower_char in BRAILLE_LETTERS:
+            braille += CAPITAL_SIGN
+
+        if char.isdigit():
+
+            if not number_mode:
+                braille += BRAILLE_NUMBERS[char]
+                number_mode = True
+            else:
+                braille += BRAILLE_NUMBERS[char]
+
+            continue
+
+        number_mode = False
+
+        if lower_char in BRAILLE_LETTERS:
+
+            braille += BRAILLE_LETTERS[lower_char]
+
+        elif char in BRAILLE_PUNCTUATION:
+
+            braille += BRAILLE_PUNCTUATION[char]
+
+        else:
+
+            braille += char
 
     return braille
